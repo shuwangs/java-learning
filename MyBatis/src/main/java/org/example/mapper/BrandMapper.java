b6ddbabfce225710097249d06f808eb513b2cce8
Shu Wang <swang3130@gatech.edu>
2026-07-26 15:21:30 -0500

    feat: implement query by condition with @Param, POJO, and map

--- MyBatis/src/main/java/org/example/mapper/BrandMapper.java
+++ MyBatis/src/main/java/org/example/mapper/BrandMapper.java
@@ -1,7 +1,9 @@
 package org.example.mapper;
+import org.apache.ibatis.annotations.Param;
 import org.example.pojo.Brand;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandMapper {
     /**
@@ -14,4 +16,27 @@ public interface BrandMapper {
      * select by id
      */
     Brand selectById(int id);
+
+    /**
+     * query by condition
+     *
+     * @param status
+     * @param companyName
+     * @param brandName
+     * @return
+
+     */
+//    List<Brand> selectByCondition (@Param("status") int status, @Param("companyName") String companyName, @Param("brandName") String brandName);
+
+    /**
+     * use the object as the params
+
+     */
+//    List<Brand> selectByCondition(Brand brand);
+
+    /**
+     * use the Map collection as the params
+
+     */
+    List<Brand> selectByCondition(Map map);
 }
--- MyBatis/src/test/java/org/example/MyBatisTest.java
+++ MyBatis/src/test/java/org/example/MyBatisTest.java
@@ -8,12 +8,16 @@ import org.junit.Test;
 
 import java.io.InputStream;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MyBatisTest {
 
     @Test
     public void testSelectAll() throws Exception {
+        System.out.println("----------------------");
+        System.out.println("testSelectAll");
         // 1. get sqlSessionFactory
         String resource = "mybatis-config.xml";
         InputStream inputStream= Resources.getResourceAsStream(resource);
@@ -31,14 +35,14 @@ public class MyBatisTest {
         List<Brand> brands = brandMapper.selectAll();
         System.out.println(brands);
 
-
         // 5. release resources
         sqlSession.close();
 
-
     }
-
+    @Test
     public void testSelectById() throws Exception {
+        System.out.println("----------------------");
+        System.out.println("testSelectById");
         int id = 1;
 
         // 1. get sqlSessionFactory
@@ -57,6 +61,53 @@ public class MyBatisTest {
 
         Brand brand = brandMapper.selectById(id);
         System.out.println(brand);
+
+        // 5. release resources
+        sqlSession.close();
+    }
+
+    @Test
+    public void testSelectByCondition() throws Exception {
+        System.out.println("----------------------");
+        System.out.println("testSelectByCondition");
+        int status = 1;
+        String companyName = "Nike";
+        String brandName = "Nike";
+
+        // normalize the input
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+        // encapsulate the object
+//        Brand brand = new Brand();
+//        brand.setBrandName(brandName);
+//        brand.setCompanyName(companyName);
+//        brand.setStatus(status);
+        // use map
+        Map map = new HashMap();
+        map.put("status", status);
+        map.put("brandName", brandName);
+        map.put("companyName", companyName);
+
+        // 1. get sqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream= Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        // 2. get the sqlSession object
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 3. get the Mapper interface proxy object
+
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        // 4. excute
+
+        // List <Brand> brands = brandMapper.selectByCondition(status, companyName, brandName);
+//        List <Brand> brands = brandMapper.selectByCondition(brand);
+        List <Brand> brands = brandMapper.selectByCondition(map);
+
+        System.out.println(brands);
 
         // 5. release resources
         sqlSession.close();
