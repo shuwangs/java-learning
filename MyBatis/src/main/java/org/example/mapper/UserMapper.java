a204cb7129a03e685a3668af8fd4fc518a707bee
Shu Wang <swang3130@gatech.edu>
2026-07-28 15:06:15 -0500

    feat: add annotation-based query in UserMapper

--- MyBatis/src/main/java/org/example/mapper/UserMapper.java
+++ MyBatis/src/main/java/org/example/mapper/UserMapper.java
@@ -1,8 +1,14 @@
 package org.example.mapper;
 
-import java.util.List;
+import org.apache.ibatis.annotations.Select;
 import org.example.pojo.User;
+
+import java.util.List;
 
 public interface UserMapper {
     List<User> selectAllUser();
+
+    // Annotation-based SQL mapping for simple statemnts
+    @Select("select * from tb_user where id = #{id}")
+    User selectById(int id);
 }
