fee4d8a6a84f6229a1cf742e9e02ae5792329476
Shu Wang <swang3130@gatech.edu>
2026-07-30 17:46:53 -0500

    Add Servlet demo and web project configuration

---
+++ web-demo/src/main/java/com/web/ServletDemo1.java
@@ -0,0 +1,34 @@
+package com.web;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+
+@WebServlet("/demo1")
+public class ServletDemo1 implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("Hello Servlet!");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "";
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
