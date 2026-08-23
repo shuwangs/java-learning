3357979a6bc1095f936406eb3fbfdd80998f3f8c
Shu Wang <shuw425@gmail.com>
2026-08-23 00:56:15 -0500

    explictly add a BookDao Constructor

---
+++ spring_01_quickstart/src/main/java/org/example/AppForInstanceBook.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.example.dao.BookDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AppForInstanceBook {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookDao bookDao = (BookDao) ctx.getBean("bookDao");
+
+        bookDao.save();
+
+    }
+}
+
--- spring_01_quickstart/src/main/java/org/example/dao/impl/BookDaoImpl.java
+++ spring_01_quickstart/src/main/java/org/example/dao/impl/BookDaoImpl.java
@@ -3,8 +3,14 @@ package org.example.dao.impl;
 import org.example.dao.BookDao;
 
 public class BookDaoImpl implements BookDao {
+    // Beans can get both private and public constructor without params;
+    // If no constructor is explicitly declared, Java provides a default no-argument constructor.
+
+    private BookDaoImpl() {
+        System.out.println("Book dao constructor is running");
+    }
 
     public void save() {
-        System.out.println("bookDao is saved ");
+        System.out.println("bookDao save.... ");
     }
 }
