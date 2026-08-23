88e5ed1a5ee443bf39f05fc9b3e6bd09d91832a4
Shu Wang <shuw425@gmail.com>
2026-08-23 15:57:32 -0500

    use factory bean to initialize

---
+++ spring_01_quickstart/src/main/java/org/example/factory/UserDaoFactoryBean.java
@@ -0,0 +1,17 @@
+package org.example.factory;
+
+import org.example.dao.UserDao;
+import org.example.dao.impl.UserDaoImpl;
+import org.springframework.beans.factory.FactoryBean;
+
+public class UserDaoFactoryBean implements FactoryBean<UserDao> {
+
+    public UserDao getObject() throws Exception {
+        return new UserDaoImpl();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return UserDao.class;
+    }
+}
