7ce04d8f23c3d82f4ae6816e4e65d1b0883d13bf
Shu Wang <shuw425@gmail.com>
2026-08-23 00:00:19 -0500

    bean configuration, alias, and scope

---
+++ spring_01_quickstart/src/main/java/org/example/AppForScope.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.example.dao.BookDao;
+import org.example.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AppForScope {
+    public static void main(String[] args) {
+// get IoC container
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookDao bookDao1 = (BookDao) ctx.getBean("bookDao");
+        BookDao bookDao2 = (BookDao) ctx.getBean("bookDao");
+
+        //Beam default to generate single instances
+        System.out.println(bookDao1); // org.example.dao.impl.BookDaoImpl@5ef60048
+        System.out.println(bookDao2); // org.example.dao.impl.BookDaoImpl@5ef60048
+    }
+}
