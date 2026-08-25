1ccf822be8b11eeb007a7f3bb03dff643f4d5545
Shu Wang <shuw425@gmail.com>
2026-08-25 00:36:12 -0500

    load properites with confige file.

--- spring_03_datasource/src/main/java/org/example/App.java
+++ spring_03_datasource/src/main/java/org/example/App.java
@@ -1,9 +1,8 @@
 package org.example;
 
+import org.example.dao.BookDao;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import javax.sql.DataSource;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,7 +10,7 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        DataSource dataSource = (DataSource) ctx.getBean("dataSource");
-        System.out.println(dataSource);
+        BookDao bookDao = (BookDao) ctx.getBean("bookDao");
+        bookDao.save();
     }
 }
---
+++ spring_03_datasource/src/main/java/org/example/dao/impl/BookDaoImpl.java
@@ -0,0 +1,17 @@
+package org.example.dao.impl;
+
+import org.example.dao.BookDao;
+
+public class BookDaoImpl implements BookDao {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void save() {
+        System.out.println("book dao save..." + name);
+    }
+}
+
