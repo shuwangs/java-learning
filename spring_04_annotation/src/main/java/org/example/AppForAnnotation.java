40dd8e4d01d3fe90750e199ae6f0afbde4c271da
Shu Wang <shuw425@gmail.com>
2026-08-25 23:55:01 -0500

    use componentScan package for auto annotation

--- spring_04_annotation/src/main/java/org/example/AppForAnnotation.java
+++ spring_04_annotation/src/main/java/org/example/AppForAnnotation.java
@@ -1,21 +1,24 @@
 package org.example;
 
+import org.example.config.SpringConfig;
 import org.example.dao.BookDao;
 import org.example.service.BookService;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.awt.print.Book;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class App {
+public class AppForAnnotation {
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("ApplicationContext.xml.bak");
-
+    ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
         BookDao bookDao = (BookDao) ctx.getBean("bookDao");
-
         System.out.println(bookDao);
 
         BookService bookService = (BookService) ctx.getBean(BookService.class);
-        System.out.println(bookService );
+        System.out.println(bookService);
+
     }
 }
