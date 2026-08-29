0279189723265616031e77d1dbda0a807caa2f66
Shu Wang <shuw425@gmail.com>
2026-08-29 00:24:43 -0500

    add junit

---
+++ spring_07_spring_mybatis/src/test/java/org/example/service/AccountServiceTest.java
@@ -0,0 +1,24 @@
+package org.example.service;
+
+import org.example.config.SpringConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+public class AccountServiceTest {
+    @Autowired
+    private AccountService accountService;
+    @Test
+    public  void testFindById() {
+        System.out.println(accountService.findById(1));
+    }
+
+    @Test
+    public  void testFindAll() {
+        System.out.println(accountService.findAll());
+    }
+}
