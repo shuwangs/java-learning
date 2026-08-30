28fe31bccdea9593c2bc5c1c0532545326442370
Shu Wang <shuw425@gmail.com>
2026-08-30 15:15:23 -0500

    start webapp use servlet

---
+++ spring_10_springmvc_demo/src/main/java/org/example/servlet/UserDeleteServlet.java
@@ -0,0 +1,31 @@
+package org.example.servlet;
+
+import com.sun.net.httpserver.HttpServer;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/user/delete")
+public class UserDeleteServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+        // get request
+        String name = req.getParameter("name");
+        System.out.println("servlet delete name: " + name);
+
+        // get the response
+         res.setContentType("text/json;charset=utf-8");
+        PrintWriter pw = res.getWriter();
+        pw.write("{'module': 'servlet delete'}");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        this.doGet(req, res);
+    }
+}
---
+++ spring_10_springmvc_demo/src/main/java/org/example/servlet/UserUpdateServlet.java
@@ -0,0 +1,29 @@
+package org.example.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/user/update")
+public class UserUpdateServlet extends HttpServlet{
+        protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+            // get request
+            String name = req.getParameter("name");
+            System.out.println("servlet update name: " + name);
+
+            // get the response
+            res.setContentType("text/json;charset=utf-8");
+            PrintWriter pw = res.getWriter();
+            pw.write("{'module': 'servlet update'}");
+        }
+
+        @Override
+        protected void doPost (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+            this.doGet(req, res);
+        }
+}
