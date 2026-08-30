9483b88087f63a9523b1a59527cf6d75af37b3b8
Shu Wang <shuw425@gmail.com>
2026-08-30 14:21:33 -0500

    test: verify transfer logging with REQUIRES_NEW propagation

--- spring_09_case_transfer/src/main/java/org/example/service/LogService.java
+++ spring_09_case_transfer/src/main/java/org/example/service/LogService.java
@@ -1,10 +1,11 @@
 package org.example.service;
 
 import org.apache.ibatis.annotations.Insert;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface LogService {
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @Insert("insert into tbl_log(info, create_time) values(#{info}, now())")
     void log(String out, String in, Double money);
 }
--- spring_09_case_transfer/src/main/java/org/example/service/impl/AccountServiceImpl.java
+++ spring_09_case_transfer/src/main/java/org/example/service/impl/AccountServiceImpl.java
@@ -20,8 +20,10 @@ public class AccountServiceImpl implements AccountService {
     public void transfer(String out, String in, Double money) {
         try{
             accountDao.outMoney(out, money);
+            int i = 1/0;
             accountDao.inMoney(in, money);
         } finally {
+            // must run it.
             logService.log(out, in, money);
 
         }
